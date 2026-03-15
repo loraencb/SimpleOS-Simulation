6ab43a788982e07bdce4cf115c391e76c4ddf0c4
Bobbi-P <perrybo@kean.edu>
2026-03-14 22:41:18 -0400

    Add common.h with system constants definitions

---
+++ simpleOS/common.h
@@ -0,0 +1,12 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#define MAX_PROCESSES 10
+#define MAX_PAGES 32
+#define PAGE_SIZE 4096
+
+#define MAX_FILES 10
+#define MAX_FILENAME 32
+#define MAX_FILE_CONTENT 256
+
+#endif
