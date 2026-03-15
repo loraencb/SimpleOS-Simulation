38336218c64c5423d6b4bb56130726455c498619
Bobbi-P <perrybo@kean.edu>
2026-03-14 22:39:27 -0400

    Implement main function for SimpleOS kernel simulator
    This file serves as the entry point for the SimpleOS kernel simulator, initializing subsystems and creating processes.

---
+++ simpleOS/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "process.h"
+#include "memory.h"
+#include "fs.h"
+#include "shell.h"
+
+int main(void) {
+    printf("SimpleOS Kernel Simulator Started\n");
+    printf("Initializing subsystems...\n");
+
+    init_process_table();
+    init_memory();
+    init_fs();
+
+    create_process("init", 1);
+    create_process("shell", 1);
+
+    printf("Initialization complete.\n");
+    printf("Type 'help' for available commands.\n\n");
+
+    run_shell();
+
+    return 0;
+}
