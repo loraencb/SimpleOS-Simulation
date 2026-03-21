e1bb9b449d98dc79ef69dfd5c404b3361814a929
Bobbi-P <perrybo@kean.edu>
2026-03-20 21:45:12 -0400

    Include open file table initialization
    Added initialization for open file table.

--- simpleOS/main.c
+++ simpleOS/main.c
@@ -3,6 +3,7 @@
 #include "memory.h"
 #include "fs.h"
 #include "shell.h"
+#include "syscall.h"
 
 int main(void) {
     printf("SimpleOS Kernel Simulator Started\n");
@@ -11,6 +12,7 @@ int main(void) {
     init_process_table();
     init_memory();
     init_fs();
+    init_open_file_table();
 
     create_process("init", 1);
     create_process("shell", 1);
