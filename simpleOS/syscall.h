bc621ffd90df430b749bfa0c768089e506fe7281
Bobbi-P <perrybo@kean.edu>
2026-03-20 21:44:20 -0400

    Add file operations to syscall header

--- simpleOS/syscall.h
+++ simpleOS/syscall.h
@@ -1,7 +1,10 @@
 #ifndef SYSCALL_H
 #define SYSCALL_H
 
-int sys_read_file(const char *filename);
+void init_open_file_table(void);
+int sys_open(const char *filename);
+int sys_read(int fd, char *buffer, int nbytes);
+int sys_close(int fd);
 int sys_write_file(const char *filename, const char *content);
 
 #endif
